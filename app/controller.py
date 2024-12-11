fffbac368939fbeaf2c1701090c295cf5c9e12c4
Raze955 <lluch955@gmail.com>
2024-12-11 20:39:04 +0800

    rendered college_bp blueprint to app

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,9 @@
 from .landing import landing_bp
 from .students import students_bp
+from .colleges import colleges_bp
 
 
 def register_routes(app):
     app.register_blueprint(landing_bp)
     app.register_blueprint(students_bp)
+    app.register_blueprint(colleges_bp)
