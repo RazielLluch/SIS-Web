29e927923ffaedc6c9afc2059cb5a4f2e206e1cc
Raze955 <lluch955@gmail.com>
2024-12-15 02:17:42 +0800

    some additional refactoring for the student_model

--- app/models/student_model.py
+++ app/models/student_model.py
@@ -21,46 +21,6 @@ class StudentModel(Model):
         self.gender = gender
         self.profile_picture_url = profile_picture_url
 
-    def find_by_id(self, student_id):
-        return self.read(self.table_name, conditions={'id': student_id}, fetch_one=True)
-
-    def fetch_all(self):
-        return self.read(self.table_name)
-
-    def add_student(self):
-        self.create(
-            self.table_name,
-            {
-                'id': self.id,  # Example session ID
-                'firstname': self.firstname,  # Assuming the user with ID 1 is logged in
-                'lastname': self.lastname,
-                'course': self.course,  # Example timestamp
-                'year': self.year,
-                'gender': self.gender,
-                'profile_picture_url': self.profile_picture_url
-            }
-        )
-
-    def edit(self, basis_id):
-        self_dict = self.to_dict()
-        
-        print("self_dict: ",self_dict)
-        
-        return self.update(
-            self.table_name,
-            self_dict,
-            {
-                "id": basis_id
-            }
-        )
-
-    def delete_by_ids(self, student_ids):
-        return self.delete(
-            self.table_name,
-            'id',
-            student_ids
-        )
-
     def to_dict(self):
         return {
             'id': self.id,
@@ -72,3 +32,32 @@ class StudentModel(Model):
             'profile_picture_url': self.profile_picture_url,
         }
 
+    def find_by_id(self, student_id):
+        return self.read(self.table_name, conditions={'id': student_id}, fetch_one=True)
+
+    def fetch_all(self):
+        return self.read(self.table_name)
+
+    def add(self):
+        self.create(
+            self.table_name,
+            self.to_dict()
+        )
+
+    def edit(self, basis_id):
+        self_dict = self.to_dict()
+
+        print("self_dict: ", self_dict)
+
+        return self.update(
+            self.table_name,
+            self_dict,
+            {"id": basis_id}
+        )
+
+    def delete_by_ids(self, student_ids):
+        return self.delete(
+            self.table_name,
+            'id',
+            student_ids
+        )
